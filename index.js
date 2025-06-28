01119c393d988269a1a9215407e7be1afe7df42a
mauricioarrequena <mauricio.ar.requena@gmail.com>
2025-06-28 16:57:30 -0400

    feat: updated the welcome message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+  res.send('Hello from Express, this should be deployed!');
 });
 
 app.listen(PORT, () => {
